1707f47fb5ecb603c0cddc6ded0dc411bdf18841
Yujin Im <jina0605@gmail.com>
2022-02-28 18:15:30 +0900

    edit authJwt

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -3,14 +3,16 @@ const { verifyJwt } = require('../services/auth')
 exports.authJwt = (req, res, next) => {
   const authorization = req.headers.authorization
   const accessToken = authorization ? authorization.split('Bearer ')[1] : null
-  if (!accessToken) res.status(401).send()
-  else {
+  if (!accessToken) {
+    req.uid = null
+    req.jwtMessage = 'no access token'
+  } else {
     const { ok, result, message } = verifyJwt(accessToken)
     if (ok) req.uid = result.uid
     else {
       req.uid = null
       req.jwtMessage = message
     }
-    next()
   }
+  next()
 }
