ca5ce1201629e39e4fd8509f2ac5cc2fe6a25222
Yujin Im <jina0605@gmail.com>
2022-04-09 15:45:52 +0900

    model에서 Users와 UserInfo 분리

--- app/models/Users.js
+++ app/models/Users.js
@@ -11,58 +11,15 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false,
       unique: true
     },
-    name: {
-      type: DataTypes.STRING(256),
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING(256),
-      allowNull: false
-    },
-    gender: {
-      type: DataTypes.STRING(32),
-      allowNull: false
-    },
-    birth: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    googleId: {
-      type: DataTypes.STRING(256),
-      allowNull: true
-    },
-    appleId: {
-      type: DataTypes.STRING(256),
-      allowNull: true
-    },
-    phone: {
-      type: DataTypes.STRING(32),
-      allowNull: false,
-      validate: {
-        is: /(^\+\d{1,4}-\d{1,3}-\d{3,4}-\d{3,4}$)|(^$)/i
-      }
-    },
-    major: {
-      type: DataTypes.STRING(32),
-      allowNull: false
-    },
-    majorDetail: {
-      type: DataTypes.STRING(32),
-      allowNull: false
-    },
-    school: {
-      type: DataTypes.STRING(256),
-      allowNull: false
-    },
-    graduationStatus: {
-      type: DataTypes.STRING(32),
-      allowNull: false
-    },
     credit: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    auth: {
+    role: {
+      type: DataTypes.STRING(32),
+      allowNull: false
+    },
+    deleted: {
       type: DataTypes.INTEGER,
       allowNull: false
     }
