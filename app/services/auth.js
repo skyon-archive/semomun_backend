9c9f419ceb601f03d2269e8215bdd6062cd26f2f
Yujin Im <jina0605@gmail.com>
2022-04-14 22:05:23 +0900

    애플 로그인 관련 validation 추가

--- app/services/auth.js
+++ app/services/auth.js
@@ -1,18 +1,20 @@
 const { OAuth2Client } = require('google-auth-library')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const jwksClient = require('jwks-rsa')({ jwksUri: 'https://appleid.apple.com/auth/keys' })
 const env = process.env
-const clientId = [env.CLIENT_ID_APP, env.CLIENT_ID_WEB]
-const secret = env.JWT_SECRET
+const googleClientId = [env.GOOGLE_CLIENT_ID_APP, env.GOOGLE_CLIENT_ID_WEB]
+const appleClientId = [env.APPLE_CLIENT_ID_APP, env.APPLE_CLIENT_ID_WEB]
+const jwtSecret = env.JWT_SECRET
 const salt = env.SALT
-const client = new OAuth2Client(clientId)
+const client = new OAuth2Client(googleClientId)
 const redis = require('./redis')
 
 exports.getGoogleId = async (token) => {
   try {
     const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: clientId
+      audience: googleClientId
     })
     const payload = ticket.getPayload()
     const sub = payload.sub
@@ -24,16 +26,38 @@ exports.getGoogleId = async (token) => {
 
 exports.getGoogleIdLegacy = async (token) => {
   try {
-    const { aud, sub } = jwt.decode(token)
-    return clientId.includes(aud) ? sub : null
+    const { sub } = jwt.verify(
+      token,
+      { audience: googleClientId, ignoreExpiration: true }
+    )
+    return sub
   } catch (err) {
     return null
   }
 }
 
-exports.getAppleId = async (token) => {
+const getAppleSigningKey = (kid) => {
+  return new Promise((resolve, reject) => {
+    jwksClient.getSigningKey(kid, (err, key) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(key)
+      }
+    })
+  })
+}
+
+exports.getAppleId = async (token, ignoreExpiration = false) => {
   try {
-    const sub = jwt.decode(token).sub
+    const { kid, alg } = jwt.decode(token, { complete: true }).header
+    const key = await getAppleSigningKey(kid)
+    const { sub } = jwt.verify(token, key.getPublicKey(), {
+      issuer: 'https://appleid.apple.com',
+      audience: appleClientId,
+      algorithms: [alg],
+      ignoreExpiration
+    })
     return sub
   } catch (err) {
     return null
@@ -47,8 +71,8 @@ exports.getAuthJwtKey = getAuthJwtKey
 
 exports.createJwt = async (uid, short = false) => {
   const payload = { uid }
-  const accessToken = jwt.sign(payload, secret, { algorithm: 'HS256', expiresIn: '10m' })
-  const refreshToken = jwt.sign({}, secret, { algorithm: 'HS256', expiresIn: short ? '3h' : '30d' })
+  const accessToken = jwt.sign(payload, jwtSecret, { algorithm: 'HS256', expiresIn: '10m' })
+  const refreshToken = jwt.sign({}, jwtSecret, { algorithm: 'HS256', expiresIn: short ? '3h' : '30d' })
   const accessHash = bcrypt.hashSync(accessToken, +salt)
   const redisKey = getAuthJwtKey(refreshToken)
   await redis.set(redisKey, accessHash, { EX: 60 * 60 * 24 * 30 + 15 })
@@ -56,9 +80,9 @@ exports.createJwt = async (uid, short = false) => {
   return { accessToken, refreshToken }
 }
 
-exports.verifyJwt = (token) => {
+exports.verifyJwt = (token, secret = jwtSecret) => {
   try {
-    const result = jwt.verify(token, secret)
+    const result = jwt.verify(token, jwtSecret)
     return { ok: true, result }
   } catch (e) {
     try {
