17b4c99c5f32e9f89b9d79c431e88f5f554e6b87
Yujin Im <jina0605@gmail.com>
2022-04-21 08:17:39 +0900

    migrateUsers.js에 fixSchool 구현

--- app/migrations/migrateUsers.js
+++ app/migrations/migrateUsers.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { UserInfo } = require('../models/index')
 const { createUser } = require('../services/user')
 const { readCsv } = require('./utils')
 
@@ -11,12 +12,12 @@ exports.migrateUsers = async () => {
     const cnt = users.length
     for (let i = 0; i * 100 < cnt; i += 1) {
       console.log(`i = ${i}`)
-      await Promise.all(users.slice(i * 100, (i + 1) * 100).map(async user => {
+      await Promise.all(users.slice(i * 100, (i + 1) * 100).map(async (user) => {
         await createUser({
           usernameDup: true,
           username: user.name,
           phone: '',
-          school: '',
+          school: user.school,
           major: user.major,
           majorDetail: user.majorDetail,
           favoriteTags: [],
@@ -25,6 +26,31 @@ exports.migrateUsers = async () => {
           googleId: user.appleId.length === 21 ? user.appleId : null,
           appleId: user.appleId.length === 44 ? user.appleId : null
         })
+      }))
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.fixSchool = async () => {
+  try {
+    const users = (await readCsv(
+      path.resolve(__dirname, 'users.csv'),
+      (user) => user
+    ))
+    const cnt = users.length
+    for (let i = 0; i * 100 < cnt; i += 1) {
+      console.log(`i = ${i}`)
+      await Promise.all(users.slice(i * 100, (i + 1) * 100).map(async (user) => {
+        await UserInfo.update(
+          { school: user.school },
+          { where: { appleId: user.appleId } }
+        )
+        await UserInfo.update(
+          { school: user.school },
+          { where: { googleId: user.appleId } }
+        )
       }))
     }
   } catch (err) {
--- migrate.js
+++ migrate.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 // const { migrateWorkbooks, updateSectionSize } = require('./app/migrations/migrateWorkbooks')
-const { migrateUsers } = require('./app/migrations/migrateUsers')
+// const { migrateUsers } = require('./app/migrations/migrateUsers')
+const { fixSchool } = require('./app/migrations/migrateUsers')
 
 // const wids = [169]
 // migrateWorkbooks(wids).then(() => updateSectionSize(wids))
 
-migrateUsers()
+// migrateUsers()
+
+fixSchool()
