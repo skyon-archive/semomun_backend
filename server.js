63aec2e398f11126656a222e8232bf15a226502b
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-04 12:18:52 +0900

    hotfix: 요청 크기 10mb로 제한 늘림

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const corsOptions = {
   credentials: true
 };
 app.use(cors(corsOptions))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
 require('./app/models/index')
 require('./app/services/redis')
