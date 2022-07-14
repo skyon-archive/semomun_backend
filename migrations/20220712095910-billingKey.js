59c51db8fe3ce1dbf96c1620cc6e874bebcdd55e
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-14 15:50:32 +0900

    fix: default value 수정

--- migrations/20220712095910-billingKey.js
+++ migrations/20220712095910-billingKey.js
@@ -28,7 +28,7 @@ module.exports = {
       requested_at: { type: Sequelize.DATE, allowNull: false },
       status_locale: { type: Sequelize.STRING(50), allowNull: false },
       status: { type: Sequelize.INTEGER, allowNull: false },
-      deletedAt: { type: Sequelize.DATE, allowNull: false },
+      deletedAt: { type: Sequelize.DATE, allowNull: true },
       createdAt: { type: Sequelize.DATE, allowNull: false },
       updatedAt: { type: Sequelize.DATE, allowNull: false },
     });
