343740a6c04e913e59b90efb02dd51d7d142847f
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-14 15:32:12 +0900

    hotfix: undefined Error

--- app/controllers/semopay.js
+++ app/controllers/semopay.js
@@ -52,7 +52,7 @@ exports.deleteUserBillingKey = async (req, res) => {
     });
 
     await Bootpay.getAccessToken();
-    await Bootpay.destroyBillingKey(billing_key);
+    await Bootpay.destroyBillingKey(bkInfo.billing_key);
     await bkInfo.destroy();
     res.status(204).send();
   } catch (e) {
