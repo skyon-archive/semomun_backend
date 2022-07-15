66c655199bd58218ea899753582d6a12f1121dbc
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-15 15:23:41 +0900

    fix: 충전 가능한 금액 함수 모듈화 && 수동 충전시, rechargeableSemopay 체크

--- app/controllers/semopay.js
+++ app/controllers/semopay.js
@@ -8,6 +8,12 @@ const {
   selectSemopayOrdersByUid,
 } = require('../services/semopay.js');
 
+const rechargeableSemopayController = async (uid) => {
+  const sumSemopay = await selectSemopayOrdersByUid(uid);
+  const maxChargeSemopay = process.env.MAX_CHARGE_SEMOPAY;
+  return maxChargeSemopay - (sumSemopay[0].price === null ? 0 : parseInt(sumSemopay[0].price));
+};
+
 exports.createUserBillingKey = async (req, res) => {
   const uid = req.uid;
   if (!uid) return res.status(401).json({ message: 'Invalid Token.' });
@@ -78,19 +84,26 @@ exports.bootPayWebhook = async (req, res) => {
 exports.createSemopayOrder = async (req, res) => {
   const uid = req.uid;
   if (!uid) return res.status(401).json({ message: 'Invalid Token.' });
-  const { bkid, order_name, price } = req.body;
+
+  // Validate Users
+  const userInfo = await selectUsersByUid(uid);
+  if (!userInfo) return res.status(404).json({ message: 'User does not exist.' });
+  if (userInfo.deletedAt) return res.status(403).json({ message: 'Already deleted User.' });
 
   // Origin Values => billing_key, order_name, order_id, price
   // Expected Values => bkid, order_name, price
+  const { bkid, order_name, price } = req.body;
+
+  // Validate rechargeable Semopay
+  const rechargeableSemopay = await rechargeableSemopayController(uid);
+  if (price > rechargeableSemopay)
+    return res.status(403).json({ message: 'The chargeable amount has been exceeded.' });
 
   // Validate UserBillingKeys
   const bkInfo = await selectAnUserBillingKeyByInfo(bkid, uid);
   if (!bkInfo) return res.status(404).json({ message: 'Invalid Bk Info.' });
   if (bkInfo.deletedAt) return res.status(409).json({ message: 'Already deleted Info.' });
   const billing_key = bkInfo.billing_key;
-
-  // Validate Users
-  //   Users.findOne({where: {uid}})
 
   try {
     Bootpay.setConfiguration({
@@ -134,14 +147,7 @@ exports.getSemopay = async (req, res) => {
   if (!userInfo) return res.status(404).json({ message: 'User does not exist.' });
   if (userInfo.deletedAt) return res.status(403).json({ message: 'Already deleted User.' });
 
-  //   console.log('# ----- User Info ----- #');
-  //   console.log(userInfo);
   const currentSemopay = userInfo.credit;
-  //   console.log('Current Semopay =', currentSemopay);
-
-  const sumSemopay = await selectSemopayOrdersByUid(uid);
-  const maxChargeSemopay = process.env.MAX_CHARGE_SEMOPAY;
-  const rechargeableSemopay =
-    maxChargeSemopay - (sumSemopay[0].price === null ? 0 : parseInt(sumSemopay[0].price));
+  const rechargeableSemopay = await rechargeableSemopayController(uid);
   res.status(200).json({ currentSemopay, rechargeableSemopay });
 };
