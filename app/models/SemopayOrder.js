67d569a868e3cd51724521e0cdcbd676fbc3899c
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-15 16:27:07 +0900

    fix: 추가로 받아야하는 정보, 모델 최신화(SemopayOrder Model)

--- app/models/SemopayOrder.js
+++ app/models/SemopayOrder.js
@@ -27,6 +27,17 @@ module.exports = function (sequelize, DataTypes) {
       purchased_at: { type: DataTypes.DATE, allowNull: false },
       requested_at: { type: DataTypes.DATE, allowNull: false },
       card_data: { type: DataTypes.JSON, allowNull: true },
+      cancelled_price: { type: DataTypes.INTEGER, allowNull: true },
+      cancelled_tax_free: { type: DataTypes.INTEGER, allowNull: true },
+      company_name: { type: DataTypes.STRING(50), allowNull: true },
+      gateway_url: { type: DataTypes.STRING(500), allowNull: true },
+      metadata: { type: DataTypes.JSON, allowNull: true },
+      sandbox: { type: DataTypes.BOOLEAN, allowNull: true },
+      method_origin: { type: DataTypes.STRING(50), allowNull: true },
+      escrow_status_locale: { type: DataTypes.STRING(50), allowNull: true },
+      escrow_status: { type: DataTypes.STRING(50), allowNull: true },
+      status_locale: { type: DataTypes.STRING(50), allowNull: true },
+      status: { type: DataTypes.INTEGER, allowNull: true },
     },
     { sequelize, tableName: 'SemopayOrder' }
   );
