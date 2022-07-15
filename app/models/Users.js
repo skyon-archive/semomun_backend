ac8ec7c7aa4eecc73da4bbfa80e71da605add1b0
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-15 17:32:43 +0900

    fix: 자동충전을 위한 Users 테이블 수정

--- app/models/Users.js
+++ app/models/Users.js
@@ -1,40 +1,27 @@
 module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('Users', {
-    uid: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
+  return sequelize.define(
+    'Users',
+    {
+      uid: { autoIncrement: true, type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
+      username: { type: DataTypes.STRING(256), allowNull: false, unique: true },
+      credit: { type: DataTypes.INTEGER, allowNull: false },
+      role: { type: DataTypes.STRING(32), allowNull: false },
+      deleted: { type: DataTypes.INTEGER, allowNull: false },
+      isAutoCharge: { type: DataTypes.BOOLEAN, allowNull: true },
+      lessThenAmount: { type: DataTypes.INTEGER, allowNull: true },
+      chargeAmount: { type: DataTypes.INTEGER, allowNull: true },
     },
-    username: {
-      type: DataTypes.STRING(256),
-      allowNull: false,
-      unique: true
-    },
-    credit: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.STRING(32),
-      allowNull: false
-    },
-    deleted: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+    {
+      sequelize,
+      tableName: 'Users',
+      indexes: [
+        {
+          name: 'PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [{ name: 'uid' }],
+        },
+      ],
     }
-  }, {
-    sequelize,
-    tableName: 'Users',
-    indexes: [
-      {
-        name: 'PRIMARY',
-        unique: true,
-        using: 'BTREE',
-        fields: [
-          { name: 'uid' }
-        ]
-      }
-    ]
-  })
-}
+  );
+};
---
+++ migrations/20220715082356-alterUsersTableForAutoCharge.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'isAutoCharge', {
+      type: Sequelize.BOOLEAN,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('Users', 'lessThenAmount', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('Users', 'chargeAmount', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'isAutoCharge');
+    await queryInterface.removeColumn('Users', 'lessThenAmount');
+    await queryInterface.removeColumn('Users', 'chargeAmount');
+  },
+};
