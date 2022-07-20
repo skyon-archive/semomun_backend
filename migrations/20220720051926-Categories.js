062fdb8567b9edcd25365c5e0b73078174171a7a
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-20 15:28:13 +0900

    add: Categories Table

--- migrations/20220720051926-Categories.js
+++ migrations/20220720051926-Categories.js
@@ -6,15 +6,16 @@ module.exports = {
       'Categories',
       {
         cid: { type: Sequelize.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true, comment: '태그 카테고리 고유 아이디' },
-        name: { type: Sequelize.STRING(30), allowNull: false, comment: '카테고리 이름' },
-        createdAt: { type: Sequelize.DATE, allowNull: false, comment: '생성일시' },
-        updatedAt: { type: Sequelize.DATE, allowNull: false, comment: '수정일시' },
+        name: { type: Sequelize.STRING(32), allowNull: false, comment: '카테고리 이름' },
+        createdAt: { type: Sequelize.DATE, allowNull: false, comment: '생성 일시' },
+        updatedAt: { type: Sequelize.DATE, allowNull: false, comment: '수정 일시' },
       },
       { comment: '태그 카테고리', charset: 'utf8mb4', collate: 'utf8mb4_unicode_ci' }
     );
   },
 
   down: async (queryInterface, Sequelize) => {
-    queryInterface.dropTable('Categories');
+    // Drop Categories Table
+    await queryInterface.dropTable('Categories');
   },
 };
