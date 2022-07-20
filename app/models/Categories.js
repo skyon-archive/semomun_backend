faa329af645b58739336074960caa5d728c0a87d
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-20 15:49:54 +0900

    delete: datetime fields in Categories Model

--- app/models/Categories.js
+++ app/models/Categories.js
@@ -10,8 +10,6 @@ module.exports = function (sequelize, DataTypes) {
         comment: '태그 카테고리 고유 아이디',
       },
       name: { type: DataTypes.STRING(32), allowNull: false, comment: '카테고리 이름' },
-      createdAt: { type: DataTypes.DATE, allowNull: false, comment: '생성 일시' },
-      updatedAt: { type: DataTypes.DATE, allowNull: false, comment: '수정 일시' },
     },
     { sequelize, tableName: 'Categories' }
   );
