0ae9767a5b32e2c112ed769b13a52da59f44f40f
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-21 15:38:05 +0900

    style: Align

--- app/services/tags.js
+++ app/services/tags.js
@@ -1,15 +1,16 @@
-const { Tags, FavoriteTags, sequelize } = require('../models/index')
+const { Tags, FavoriteTags, sequelize } = require('../models/index');
 
 exports.getTagsOrderBy = async (orderBy) => {
-  const order = orderBy === 'popularity'
-    ? [
-        [sequelize.literal('UserCount'), 'DESC'],
-        [sequelize.literal('WorkbookCount'), 'DESC'],
-        ['tid', 'ASC']
-      ]
-    : orderBy === 'name'
+  const order =
+    orderBy === 'popularity'
+      ? [
+          [sequelize.literal('UserCount'), 'DESC'],
+          [sequelize.literal('WorkbookCount'), 'DESC'],
+          ['tid', 'ASC'],
+        ]
+      : orderBy === 'name'
       ? [['name', 'ASC']]
-      : [['tid', 'ASC']]
+      : [['tid', 'ASC']];
   const { count, rows } = await Tags.findAndCountAll({
     attributes: {
       include: [
@@ -17,25 +18,25 @@ exports.getTagsOrderBy = async (orderBy) => {
           sequelize.literal(
             '(SELECT COUNT(*) FROM FavoriteTags WHERE FavoriteTags.tid = Tags.tid)'
           ),
-          'UserCount'
+          'UserCount',
         ],
         [
           sequelize.literal(
             '(SELECT COUNT(*) FROM WorkbookTags WHERE WorkbookTags.tid = Tags.tid)'
           ),
-          'WorkbookCount'
-        ]
-      ]
+          'WorkbookCount',
+        ],
+      ],
     },
-    order
-  })
-  return { count, tags: rows }
-}
+    order,
+  });
+  return { count, tags: rows };
+};
 
 exports.getTagsByUid = (uid) => {
   return FavoriteTags.findAll({
     where: { uid },
     include: 'tid_Tag',
-    order: [['createdAt', 'ASC']]
-  })
-}
+    order: [['createdAt', 'ASC']],
+  });
+};
