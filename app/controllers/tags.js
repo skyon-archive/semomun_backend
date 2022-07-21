680d8530486346bce0269f4ea2eb7035c4f398a7
minho-lee0716 <minho.lee0716@gmail.com>
2022-07-21 17:08:38 +0900

    style: Align

--- app/controllers/tags.js
+++ app/controllers/tags.js
@@ -1,58 +1,60 @@
-const { FavoriteTags, sequelize } = require('../models')
-const { getTagsOrderBy, getTagsByUid } = require('../services/tags')
-const { BadRequest } = require('../errors')
+const { FavoriteTags, sequelize } = require('../models');
+const { getTagsOrderBy, getTagsByUid } = require('../services/tags');
+const { BadRequest } = require('../errors');
 
 exports.getTags = async (req, res) => {
   try {
-    const { order } = req.query
-    const result = await getTagsOrderBy(order)
-    res.json(result)
+    const { order } = req.query;
+    const result = await getTagsOrderBy(order);
+    res.json(result);
   } catch (err) {
-    console.log(err)
-    res.status(500).send()
+    console.log(err);
+    res.status(500).send();
   }
-}
+};
 
 exports.getMyTags = async (req, res) => {
   try {
-    if (!req.uid) return res.status(401).send(req.jwtMessage)
-    const result = await getTagsByUid(req.uid)
-    res.json(result.map(({ tid_Tag, createdAt }) => ({
-      tid: tid_Tag.tid,
-      name: tid_Tag.name,
-      createdAt,
-      category: tid_Tag.category
-    })))
+    if (!req.uid) return res.status(401).send(req.jwtMessage);
+    const result = await getTagsByUid(req.uid);
+    res.json(
+      result.map(({ tid_Tag, createdAt }) => ({
+        tid: tid_Tag.tid,
+        name: tid_Tag.name,
+        createdAt,
+        category: tid_Tag.category,
+      }))
+    );
   } catch (err) {
-    console.log(err)
-    res.status(500).send()
+    console.log(err);
+    res.status(500).send();
   }
-}
+};
 
 exports.updateMyTags = async (req, res) => {
   try {
-    if (!req.uid) return res.status(401).send(req.jwtMessage)
-    const prevTags = (await getTagsByUid(req.uid)).map(({ tid }) => tid)
-    const newTags = req.body
-    if (!Array.isArray(newTags)) throw new BadRequest('request body is not array')
-    const toDelete = prevTags.filter((tag) => !newTags.includes(tag))
-    const toCreate = newTags.filter((tag) => !prevTags.includes(tag))
+    if (!req.uid) return res.status(401).send(req.jwtMessage);
+    const prevTags = (await getTagsByUid(req.uid)).map(({ tid }) => tid);
+    const newTags = req.body;
+    if (!Array.isArray(newTags)) throw new BadRequest('request body is not array');
+    const toDelete = prevTags.filter((tag) => !newTags.includes(tag));
+    const toCreate = newTags.filter((tag) => !prevTags.includes(tag));
     await sequelize.transaction(async (transaction) => {
       await FavoriteTags.destroy({
         where: { uid: req.uid, tid: toDelete },
-        transaction
-      })
+        transaction,
+      });
       await FavoriteTags.bulkCreate(
         toCreate.map((tid) => ({ tid, uid: req.uid })),
         { transaction }
-      )
-    })
-    res.json({})
+      );
+    });
+    res.json({});
   } catch (err) {
-    if (err instanceof BadRequest) res.status(400).send(err.message)
+    if (err instanceof BadRequest) res.status(400).send(err.message);
     else {
-      console.log(err)
-      res.status(500).send()
+      console.log(err);
+      res.status(500).send();
     }
   }
-}
+};
